2f36ed69f1da81ac827d07f5c992ab3e4acf2b1e
Jaakko Laurikainen <jla@wapice.com>
2024-12-07 14:06:25 +0200

    autosampler improvements

--- package/samplers/auto_sampler/_sampler.py
+++ package/samplers/auto_sampler/_sampler.py
@@ -114,16 +114,21 @@ class AutoSampler(BaseSampler):
 
     def _include_conditional_param(self, study: Study) -> bool:
         trials = study._get_trials(
-            deepcopy=False, states=(TrialState.COMPLETE, TrialState.PRUNED), use_cache=True
+            deepcopy=False,
+            states=(TrialState.COMPLETE, TrialState.PRUNED),
+            use_cache=True,
         )
         if len(trials) <= 2:
             return False
 
         param_key = set(trials[1].params)
-        return any(param_key != set(t.params) for t in trials)
+        return any(param_key != set(t.params) for t in trials[2:])
 
     def _determine_multi_objective_sampler(
-        self, study: Study, trial: FrozenTrial, search_space: dict[str, BaseDistribution]
+        self,
+        study: Study,
+        trial: FrozenTrial,
+        search_space: dict[str, BaseDistribution],
     ) -> BaseSampler:
         # TODO(nabenabe): Add more efficient heuristic for MO.
         if isinstance(self._sampler, NSGAIISampler):
@@ -133,7 +138,10 @@ class AutoSampler(BaseSampler):
         return NSGAIISampler(constraints_func=self._constraints_func, seed=seed)
 
     def _determine_single_objective_sampler(
-        self, study: Study, trial: FrozenTrial, search_space: dict[str, BaseDistribution]
+        self,
+        study: Study,
+        trial: FrozenTrial,
+        search_space: dict[str, BaseDistribution],
     ) -> BaseSampler:
         if isinstance(self._sampler, TPESampler):
             return self._sampler
@@ -163,7 +171,9 @@ class AutoSampler(BaseSampler):
         )
         if (
             self._constraints_func is not None
-            or any(isinstance(d, CategoricalDistribution) for d in search_space.values())
+            or any(
+                isinstance(d, CategoricalDistribution) for d in search_space.values()
+            )
             or self._include_conditional_param(study)
         ):
             # Use ``TPESampler`` if search space includes conditional or categorical parameters.
@@ -192,7 +202,9 @@ class AutoSampler(BaseSampler):
                 complete_trials.sort(key=lambda trial: trial.datetime_complete)
                 warm_start_trials = complete_trials[: self._N_COMPLETE_TRIALS_FOR_CMAES]
                 return CmaEsSampler(
-                    seed=seed, source_trials=warm_start_trials, warn_independent_sampling=True
+                    seed=seed,
+                    source_trials=warm_start_trials,
+                    warn_independent_sampling=True,
                 )
         else:
             return TPESampler(
@@ -206,7 +218,10 @@ class AutoSampler(BaseSampler):
         return self._sampler  # No update happens to self._sampler.
 
     def _determine_sampler(
-        self, study: Study, trial: FrozenTrial, search_space: dict[str, BaseDistribution]
+        self,
+        study: Study,
+        trial: FrozenTrial,
+        search_space: dict[str, BaseDistribution],
     ) -> BaseSampler:
         if len(study.directions) == 1:
             return self._determine_single_objective_sampler(study, trial, search_space)
@@ -219,7 +234,10 @@ class AutoSampler(BaseSampler):
         return self._sampler.infer_relative_search_space(study, trial)
 
     def sample_relative(
-        self, study: Study, trial: FrozenTrial, search_space: dict[str, BaseDistribution]
+        self,
+        study: Study,
+        trial: FrozenTrial,
+        search_space: dict[str, BaseDistribution],
     ) -> dict[str, Any]:
         return self._sampler.sample_relative(study, trial, search_space)
 
@@ -230,14 +248,20 @@ class AutoSampler(BaseSampler):
         param_name: str,
         param_distribution: BaseDistribution,
     ) -> Any:
-        return self._sampler.sample_independent(study, trial, param_name, param_distribution)
+        return self._sampler.sample_independent(
+            study, trial, param_name, param_distribution
+        )
 
     def before_trial(self, study: Study, trial: FrozenTrial) -> None:
         # NOTE(nabenabe): Sampler must be updated in this method. If, for example, it is updated in
         # infer_relative_search_space, the sampler for before_trial and that for sample_relative,
         # after_trial might be different, meaning that the sampling routine could be incompatible.
         if (
-            len(study._get_trials(deepcopy=False, states=(TrialState.COMPLETE,), use_cache=True))
+            len(
+                study._get_trials(
+                    deepcopy=False, states=(TrialState.COMPLETE,), use_cache=True
+                )
+            )
             != 0
         ):
             search_space = IntersectionSearchSpace().calculate(study)
@@ -245,7 +269,9 @@ class AutoSampler(BaseSampler):
 
         sampler_name = self._sampler.__class__.__name__
         _logger.debug(f"Sample trial#{trial.number} with {sampler_name}.")
-        study._storage.set_trial_system_attr(trial._trial_id, _SAMPLER_KEY, sampler_name)
+        study._storage.set_trial_system_attr(
+            trial._trial_id, _SAMPLER_KEY, sampler_name
+        )
         self._sampler.before_trial(study, trial)
 
     def after_trial(
@@ -256,9 +282,14 @@ class AutoSampler(BaseSampler):
         values: Sequence[float] | None,
     ) -> None:
         assert state in [TrialState.COMPLETE, TrialState.FAIL, TrialState.PRUNED]
-        if isinstance(self._sampler, RandomSampler) and self._constraints_func is not None:
+        if (
+            isinstance(self._sampler, RandomSampler)
+            and self._constraints_func is not None
+        ):
             # NOTE(nabenabe): Since RandomSampler does not handle constraints, we need to
             # separately set the constraints here.
-            _process_constraints_after_trial(self._constraints_func, study, trial, state)
+            _process_constraints_after_trial(
+                self._constraints_func, study, trial, state
+            )
 
         self._sampler.after_trial(study, trial, state, values)
